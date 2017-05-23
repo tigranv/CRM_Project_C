de4bd2a7c97975dccbbf5e5486f9214a397a3588
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-23 10:42:22 +0400

    New db project added

---
+++ CRM_Project_C/Source/CRM.Database_C/dbo/Tables/EmailLists.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[EmailLists] (
+    [EmailListID]   INT           IDENTITY (1, 1) NOT NULL,
+    [EmailListName] VARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_dbo.EmailLists] PRIMARY KEY CLUSTERED ([EmailListID] ASC)
+);
---
+++ CRM_Project_C/Source/CRM.Database_C/dbo/Tables/Templates.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Templates] (
+    [TemplateId]   INT            IDENTITY (1, 1) NOT NULL,
+    [TemplateName] NVARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([TemplateId] ASC)
+);
