73906702050c90bc9eeb91923bec8f2f1bc25c13
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-23 10:44:47 +0400

    Delete Templates.sql

--- CRM_Project_C/Source/CRM.Database/dbo/Tables/Templates.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Templates] (
-    [TemplateId]   INT            IDENTITY (1, 1) NOT NULL,
-    [TemplateName] NVARCHAR (100) NOT NULL,
-    PRIMARY KEY CLUSTERED ([TemplateId] ASC)
-);
-
