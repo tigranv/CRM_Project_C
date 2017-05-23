f888a02519556cfbfcfc8fd5c4681fa2af2543ac
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-23 11:42:36 +0400

    Stored procedure for getting contacts by pagies

---
+++ CRM_Project_C/Source/CRM.Database_C/dbo/Stored Procedures/GetByPageProc.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[GetByPageProc]
+    @startFrom int,
+    @numberOfRows int,
+    @flag bit
+AS
+BEGIN
+IF @flag = 0
+SELECT * FROM [dbo].[Contacts]
+ORDER BY ContactID ASC OFFSET @startFrom ROWS FETCH NEXT @numberOfRows ROWS ONLY
+ELSE
+SELECT * FROM [dbo].[Contacts]
+ORDER BY ContactID DESC OFFSET @startFrom ROWS FETCH NEXT @numberOfRows ROWS ONLY
+END
