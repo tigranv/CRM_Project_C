82c91d07c4396363a896fbbf224ed5b36a9886b2
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-23 09:54:46 +0400

    Methods class library project and Entity Framework class library project added

---
+++ CRM_Project_C/Source/CRM.MethodsLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.MethodsLib
+{
+    public class Class1
+    {
+    }
+}
