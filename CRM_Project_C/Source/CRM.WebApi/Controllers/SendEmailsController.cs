0d0ca9a9923c3dd065d20a55dd57152f658c4daa
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-05-24 15:01:06 +0400

    updates method

--- CRM_Project_C/Source/CRM.WebApi/Controllers/SendEmailsController.cs
+++ CRM_Project_C/Source/CRM.WebApi/Controllers/SendEmailsController.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Web.Http;
 
 namespace CRM.WebApi.Controllers
@@ -15,32 +18,23 @@ namespace CRM.WebApi.Controllers
         {
             foreach (var item in ContactsList)
             {
-                //var myMessage = new SendGrid.SendGridMessage();
-                //myMessage.AddTo("test@sendgrid.com");
-                //myMessage.From = new MailAddress("you@youremail.com", "First Last");
-                //myMessage.Subject = "Sending with SendGrid is Fun";
-                //myMessage.Text = "and easy to do anywhere, even with C#";
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.mail.yahoo.com");
 
-                //var transportWeb = new SendGrid.Web("SENDGRID_APIKEY");
-                //transportWeb.DeliverAsync(myMessage).Wait();
+                mail.From = new MailAddress("h_lusy@yahoo.com");
+                mail.To.Add("tsovinar.ghazaryan@yahoo.com"/*ContactsList[item].Email*/);
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
 
-                //MailMessage mail = new MailMessage();
-                //SmtpClient SmtpServer = new SmtpClient("smtp.sendgrid.com");
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("login", "password");
+                SmtpServer.EnableSsl = true;
 
-                //mail.From = new MailAddress("yegoryan.narek@gmail.com");
-                //mail.To.Add("yegoryan.narek@gmail.com"/*ContactsList[item].Email*/);
-                //mail.Subject = "Test Mail";
-                //mail.Body = "This is for testing SMTP mail from GMAIL";
+                ServicePointManager.ServerCertificateValidationCallback = delegate
+                (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                { return true; };
 
-                //SmtpServer.Port = 587;
-                //SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
-                //SmtpServer.EnableSsl = true;
-
-                //ServicePointManager.ServerCertificateValidationCallback = delegate
-                //(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-                //{ return true; };
-
-                //SmtpServer.Send(mail);
+                SmtpServer.Send(mail);
             }
         }
     }
