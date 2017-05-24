2cb8cd4e0855cf83cb4ebb6ca040e1216ef050c1
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-24 18:47:35 +0400

    Pulling some changies

--- CRM_Project_C/Source/CRM.MethodsLib/ParsingExcel.cs
+++ CRM_Project_C/Source/CRM.MethodsLib/ParsingExcel.cs
@@ -1,5 +1,4 @@
-﻿using OfficeOpenXml;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,33 +7,29 @@ using System.Threading.Tasks;
 
 namespace CRM.MethodsLib
 {
-    class ParsingExcel
-    {
-        public void ExcelUpload(byte[] array, string path)
-        {
-            File.WriteAllBytes(path, array);
+    //class ParsingExcel
+    ////{
+    ////    public void ExcelUpload(byte[] array, string path)
+    ////    {
+    ////        File.WriteAllBytes(path, array);
 
 
-            ExcelPackage ep = new ExcelPackage(new FileInfo(path));
-            ExcelWorksheet ws = ep.Workbook.Worksheets["Sheet1"];
+    ////        ExcelPackage ep = new ExcelPackage(new FileInfo(path));
+    ////        ExcelWorksheet ws = ep.Workbook.Worksheets["Sheet1"];
 
-            List<string> domains = new List<string>();
-            for (int rw = 1; rw <= ws.Dimension.End.Row; rw++)
-            {
-                for (int cl = 1; cl < ws.Dimension.End.Column; cl++)
-                {
-                    if (ws.Cells[rw, cl].Value != null)
-                    {
-                        domains.Add(ws.Cells[rw, cl].Value.ToString());
-                        Console.Write(ws.Cells[rw, cl].Value + ", ");
-                    }
-                }
-                Console.WriteLine();
-            }
-
-
-
-           
-        }
-    }
+    ////        List<string> domains = new List<string>();
+    ////        for (int rw = 1; rw <= ws.Dimension.End.Row; rw++)
+    ////        {
+    ////            for (int cl = 1; cl < ws.Dimension.End.Column; cl++)
+    ////            {
+    ////                if (ws.Cells[rw, cl].Value != null)
+    ////                {
+    ////                    domains.Add(ws.Cells[rw, cl].Value.ToString());
+    ////                    Console.Write(ws.Cells[rw, cl].Value + ", ");
+    ////                }
+    ////            }
+    ////            Console.WriteLine();
+    ////        }           
+    ////    }
+    //}
 }
