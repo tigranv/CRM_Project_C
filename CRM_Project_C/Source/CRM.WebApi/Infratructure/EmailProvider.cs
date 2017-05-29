3d55787a0803a960fcee5742768aa3f47b313871
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-05-29 22:38:09 +0400

    Demo version complited

--- CRM_Project_C/Source/CRM.WebApi/Infratructure/EmailProvider.cs
+++ CRM_Project_C/Source/CRM.WebApi/Infratructure/EmailProvider.cs
@@ -5,28 +5,39 @@ using System.Net.Mail;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using System.IO;
 
 namespace CRM.WebApi.Infrastructure
 {
     public static class EmailProvider
     {
+        private static string Replace(string templateText, Contact c)
+        {
+            return templateText.Replace("{FullName}", c.FullName).Replace("{CompanyName}", c.CompanyName);
+        }
+
         public static void SendEmailToContacts(List<Contact> ContactsToSend, int TamplateId)
         {
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.mail.yahoo.com");
-            foreach (var item in ContactsToSend)
-            {
-                mail.From = new MailAddress("h_lusy@yahoo.com");
-                mail.To.Add(string.Join(",", ContactsToSend.Select(x => x.Email)));
-                mail.Subject = "Test Mail";
-                mail.Body = "This is for testing SMTP mail from Bet-C group";
+            string path = @"C:\Users\Tigran PC\Desktop\Templates\NewYear.html";
+            string templateContext = File.ReadAllText(path);
 
+
+            foreach (var contact in ContactsToSend)
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.mail.yahoo.com");
+                mail.From = new MailAddress("h_lusy@yahoo.com");
+                mail.To.Add(contact.Email);
+                //mail.To.Add(string.Join(",", ContactsToSend.Select(x => x.Email)));
+                mail.Subject = "Test Mail";
+                mail.Body = Replace(templateContext, contact);
+                mail.IsBodyHtml = true;
                 ServicePointManager.ServerCertificateValidationCallback = delegate
                 (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
                 { return true; };
-            }
 
-            SmtpServer.Send(mail);
+                SmtpServer.Send(mail);
+            }
         }
     }
 }
