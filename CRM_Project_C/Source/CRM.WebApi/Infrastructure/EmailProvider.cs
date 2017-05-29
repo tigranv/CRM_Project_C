78d59bf634ed765ff6f3ced928037cdc319605ba
Lusine <Lusine@LUSINE-PC>
2017-05-29 16:50:52 +0400

    Templates addition

--- CRM_Project_C/Source/CRM.WebApi/Infrastructure/EmailProvider.cs
+++ CRM_Project_C/Source/CRM.WebApi/Infrastructure/EmailProvider.cs
@@ -8,13 +8,24 @@ using System.Net.Mail;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Net.Security;
+using System.IO;
 
 namespace CRM.WebApi.Infrastructure
 {
     public class EmailProvider
     {
+        string path = @"C:\Users\Lusine\Desktop\Templates\NewYear.html";
         public void SendEmailToContacts(List<Contact> ContactsToSend, int TamplateId)
         {
+
+            string templateContext = File.ReadAllText(path);
+            foreach (var item in ContactsToSend)
+            {
+                templateContext.Replace("{FullName}", item.FullName)
+                    .Replace("{CompanyName}", item.CompanyName);
+            }
+            
+
             MailMessage mail = new MailMessage();
             SmtpClient SmtpServer = new SmtpClient("smtp.mail.yahoo.com");
             foreach (var item in ContactsToSend)
@@ -22,7 +33,7 @@ namespace CRM.WebApi.Infrastructure
                 mail.From = new MailAddress("h_lusy@yahoo.com");
                 mail.To.Add(string.Join(",", ContactsToSend.Select(x => x.Email)));
                 mail.Subject = "Test Mail";
-                mail.Body = "This is for testing SMTP mail from Bet-C group";
+                mail.Body = templateContext;
 
                 ServicePointManager.ServerCertificateValidationCallback = delegate
                 (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
